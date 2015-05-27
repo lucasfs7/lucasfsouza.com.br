cd18ee752f85883bb182899176e0a0976ac74499
lucasfs7 <lucasfs7@gmail.com>
2015-05-27 18:25:30 -0300

    remove logger

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
 
-app.use(express.logger());
-app.use(express.static(__dirname + '/build'));
+app.use(express.static(__dirname + '/www'));
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
